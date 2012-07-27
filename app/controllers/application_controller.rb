c6b8d6ec260e318e8cb14a77a0cda6ec314dac35
John Gesimondo <john@jmondo.com>
2012-07-27 02:13:23 -0400

    reload rails admin in dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,23 @@ class ApplicationController < ActionController::Base
     return false
   end
 
+  before_filter :reload_rails_admin, if: :rails_admin_path?
+
+  private
+
+  def reload_rails_admin
+    models = %W(User UserProfile)
+    models.each do |m|
+      RailsAdmin::Config.reset_model(m)
+    end
+    RailsAdmin::Config::Actions.reset
+
+    load("#{Rails.root}/config/initializers/rails_admin.rb")
+  end
+
+  def rails_admin_path?
+    controller_path =~ /rails_admin/ && Rails.env == "development"
+  end
+
+
 end
