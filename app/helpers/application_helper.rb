a0dae020747d222fe46f341061a9d63f97f9408e
John Gesimondo <john@jmondo.com>
2012-07-26 22:03:59 -0400

    bootstrap flash keys, styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
+  KEYS = {
+    notice: :success,
+    alert:  :error
+  }
+
   def body_class
     "#{controller.controller_name} #{controller.controller_name}-#{controller.action_name}"
+  end
+
+  def boostrap_flash_key(key)
+    KEYS[key]
   end
 end
