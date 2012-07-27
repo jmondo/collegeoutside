fe4697470d5b238b01b5e5b042f41b71795b55fa
John Gesimondo <john@jmondo.com>
2012-07-27 02:14:16 -0400

    allow writers to access dashboard, manage unpublished articles, list schools

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,11 @@ class Ability
       can :manage, :all
       can :access, :rails_admin   # grant access to rails_admin
       can :dashboard
+    elsif user.writer?
+      can :access, :rails_admin   # grant access to rails_admin
+      can :dashboard
+      can :manage, Article, published_at: nil # TODO: add school_id
+      can :index, School # so they can choose school from select in article/create
     end
   end
 
