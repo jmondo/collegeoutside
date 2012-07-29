ad92a74f9236c63c9ec048d3cbf4af64c3185573
John Gesimondo <john@jmondo.com>
2012-07-29 14:36:22 -0400

    fix ability/application nav

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,8 +6,10 @@ class Ability
     if user.chief?
       can :manage, :all
       can :access, :rails_admin   # grant access to rails_admin
+      can :show, :writer_dashboard
       can :dashboard
     elsif user.writer?
+      can :show, :writer_dashboard
       can :manage, Article, {user_id: user.id, published_at: nil}
     end
   end
