cac7049da789927fdabb92901a309ded33ac4ab7
John Gesimondo <john@jmondo.com>
2012-09-04 19:14:04 -0700

    simple format the body, do not reinvite the user, and strip the caption tags from the body

--- lib/importer/data_importer.rb
+++ lib/importer/data_importer.rb
@@ -2,13 +2,14 @@ require 'hpricot'
 module Importer
   class DataImporter
     class << self
-
       def import_author_emails
         (file/'wp:author').each do |a|
           email = (a/'wp:author_email').text
           login = (a/'wp:author_login').text
           author_hash[login] = email
-          User.invite!(email: email)
+          unless User.find_by_email(email)
+            User.invite!(email: email)
+          end
         end
       end
 
@@ -23,9 +24,11 @@ module Importer
           login = (a/'dc:creator').text
           art = Article.find_or_create_by_title(title)
 
+          raw_body = (a/'content:encoded').text + "\n\nOriginally published #{(a/'pubDate').text}"
+          no_caption_body = Formatter.new.remove_captions raw_body
 
           art.user = User.find_by_email(author_hash[login])
-          art.body = (a/'content:encoded').text
+          art.body = Formatter.new.simple_format no_caption_body
           art.school = import_school
           art.save!(validate: false)
         end
@@ -42,6 +45,14 @@ module Importer
         Hpricot::XML(file)
       end
 
+    end
+  end
+
+  class Formatter
+    include ActionView::Helpers::TextHelper
+
+    def remove_captions(string)
+      string.gsub(/\[caption[^\]]*\]/, '').gsub("[/caption]",'')
     end
   end
 end
--- spec/lib/importer/data_importer_spec.rb
+++ spec/lib/importer/data_importer_spec.rb
@@ -26,8 +26,20 @@ describe Importer::DataImporter do
       importer.import_articles
       a = Article.order(:created_at).first
       a.title.should eql("Welcome!")
-      a.body.should match("Hello and Welcome to College Outside!")
+      a.body.should match("<p>Hello and Welcome to College Outside!</p>")
       a.user.should == User.find_by_email('slockwood09@gmail.com')
+    end
+    it "adds the original publish date to end of article" do
+      importer.import_articles
+      a = Article.order(:created_at).first
+      a.body.index("<p>Originally published Wed, 21 Dec 2011 20:40:16 +0000</p>").should be_present
+    end
+    it "removes captions from images" do
+      importer.import_articles
+      a = Article.find_by_title("What to Wear: Winter Hiking")
+      a.body.index('[caption id="attachment_55" align="alignright" width="130" caption="Patagonia R1"]').should be_nil
+      a.body.index('[/caption]').should be_nil
+      a.body.index('<img class="wp-image-55  " title="35607541" src="http://www.collegeoutside.com/wp-content/uploads/2011/12/35607541-300x300.jpg" alt="" width="130" height="130" />').should_not be_nil
     end
   end
 end
