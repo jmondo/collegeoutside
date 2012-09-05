1679620076551093d49cfe03f805edbce6c4736a
John Gesimondo <john@jmondo.com>
2012-09-04 18:06:50 -0700

    more imports

--- lib/importer/data_importer.rb
+++ lib/importer/data_importer.rb
@@ -6,11 +6,36 @@ module Importer
       def import_author_emails
         (file/'wp:author').each do |a|
           email = (a/'wp:author_email').text
+          login = (a/'wp:author_login').text
+          author_hash[login] = email
           User.invite!(email: email)
         end
       end
 
+      def author_hash
+        @@author_hash ||= {}
+      end
+
+      def import_articles
+        import_author_emails
+        (file/'item').each do |a|
+          title = (a/'title').text
+          login = (a/'dc:creator').text
+          art = Article.find_or_create_by_title(title)
+
+
+          art.user = User.find_by_email(author_hash[login])
+          art.body = (a/'content:encoded').text
+          art.school = import_school
+          art.save!(validate: false)
+        end
+      end
+
       protected
+
+      def import_school
+        @@school ||= School.find_or_create_by_name("Import School")
+      end
 
       def file
         file = File.open(Rails.root.join 'lib/importer/collegeoutside.wordpress.2012-08-15.xml')
--- spec/lib/importer/data_importer_spec.rb
+++ spec/lib/importer/data_importer_spec.rb
@@ -2,15 +2,32 @@ require 'spec_helper'
 
 describe Importer::DataImporter do
   let(:importer) { Importer::DataImporter }
-  it "imports author names" do
-    importer.import_author_emails
-    User.count.should > 0
+  describe "authors" do
+    it "imports author names" do
+      importer.import_author_emails
+      User.count.should > 0
+    end
+
+    it "doesn't reimport authors if email already exists" do
+      importer.import_author_emails
+      user_count = User.count
+      importer.import_author_emails
+      User.count.should == user_count
+    end
+
+    it "creates a hash of email => creator" do
+      importer.import_author_emails
+      importer.author_hash["collegeoutside"].should == "slockwood09@gmail.com"
+    end
   end
 
-  it "doesn't reimport authors if email already exists" do
-    importer.import_author_emails
-    user_count = User.count
-    importer.import_author_emails
-    User.count.should == user_count
+  describe "posts" do
+    it "imports the post title, body, author" do
+      importer.import_articles
+      a = Article.order(:created_at).first
+      a.title.should eql("Welcome!")
+      a.body.should match("Hello and Welcome to College Outside!")
+      a.user.should == User.find_by_email('slockwood09@gmail.com')
+    end
   end
 end
