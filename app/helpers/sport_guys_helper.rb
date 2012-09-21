7804501d1cb7295a0725b31ea8589ecf82e69f85
John Gesimondo <john@jmondo.com>
2012-09-20 22:15:51 -0700

    mini sport guys, article page (metadata and content)

--- app/helpers/sport_guys_helper.rb
+++ app/helpers/sport_guys_helper.rb
@@ -1,8 +1,16 @@
 module SportGuysHelper
   def sport_guy(activity_name)
+    activity_name = activity_name.titleize
     content_tag(:div, class: "sport-guy") do
       image_tag("sport-icons/#{activity_name}-Badge_Small.png", alt: "#{activity_name} Sport Badge") +
         content_tag(:div, "", class: "sport-guy-opacity")
+    end
+  end
+
+  def mini_sport_guy(activity_name)
+    activity_name = activity_name.titleize
+    content_tag(:div, class: "sport-guy sport-guy-mini") do
+      image_tag("sport-icons/#{activity_name}-Badge_Small.png", alt: "#{activity_name} Sport Badge")
     end
   end
 end
