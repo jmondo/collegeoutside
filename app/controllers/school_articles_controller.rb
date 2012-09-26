488b76af70c7c9c42eb41ef5241373cc666c6770
John Gesimondo <john@jmondo.com>
2012-09-25 19:30:32 -0700

    thumbnails for school page, list of articles by school

--- app/controllers/school_articles_controller.rb
+++ app/controllers/school_articles_controller.rb
@@ -6,11 +6,23 @@ class SchoolArticlesController < ApplicationController
     instance_name: "article"
   belongs_to :school
 
-  helper_method :featured_article
+  helper_method :thumbnail_articles
 
   protected
 
-  def featured_article
-    @featured_article ||= @school.articles.featured.first || @school.articles.published.first
+  def thumbnail_articles
+    # first featured, first sponsored article, second featured article,
+    # fill others with regular articles, max 6
+    @thumbnail_articles ||= [
+      end_of_association_chain.featured.first,
+      end_of_association_chain.sponsored.first,
+      end_of_association_chain.featured.offset(1).first,
+      end_of_association_chain.published_no_flags.limit(6)
+    ].flatten.compact[0..5]
   end
+
+  def collection
+    @articles ||= end_of_association_chain.published
+  end
+
 end
--- app/uploaders/cover_photo_uploader.rb
+++ app/uploaders/cover_photo_uploader.rb
@@ -6,6 +6,10 @@ class CoverPhotoUploader < BaseUploader
     process resize_to_fill: [270, 192]
   end
 
+  version :double_wide_cover do
+    process resize_to_fill: [570, 192]
+  end
+
   version :medium_cover do
     process resize_to_fill: [370, 263]
   end
@@ -19,3 +23,7 @@ class CoverPhotoUploader < BaseUploader
   end
 
 end
+
+# Article.all.each do |art|
+#   art.cover_photo.recreate_versions! if art.cover_photo.present?
+# end
