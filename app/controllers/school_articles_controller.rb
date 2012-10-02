5d05b510b6566e00a5c80a331242c503fcdbf378
John Gesimondo <john@jmondo.com>
2012-10-02 13:45:39 -0700

    show up to 18 articles on school page, show rest of them as titles in sidebar

--- app/controllers/school_articles_controller.rb
+++ app/controllers/school_articles_controller.rb
@@ -11,18 +11,23 @@ class SchoolArticlesController < ApplicationController
   protected
 
   def thumbnail_articles
-    # first featured, first sponsored article, second featured article,
-    # fill others with regular articles, max 6
-    @thumbnail_articles ||= [
-      end_of_association_chain.featured.first,
-      end_of_association_chain.sponsored.first,
-      end_of_association_chain.featured.offset(1).first,
-      end_of_association_chain.published_no_flags.limit(6)
-    ].flatten.compact[0..5]
+    unless @thumbnail_articles
+      # first featured, first sponsored article, second featured article,
+      # fill others with regular articles, max 6
+      feature = end_of_association_chain.featured.limit(1)
+      sponsor = end_of_association_chain.sponsored.limit(1)
+      feature2 = end_of_association_chain.featured.offset(1).first
+      special_articles = [feature, sponsor, feature2].flatten.compact
+      qty_normal = 18 - special_articles.count
+      normal_articles = end_of_association_chain.published_no_flags.limit(qty_normal)
+      @thumbnail_articles ||= special_articles + normal_articles
+    end
+    @thumbnail_articles
   end
 
   def collection
-    @articles ||= end_of_association_chain.published
+    @articles ||= end_of_association_chain.published.
+      where(Article.arel_table[:id].not_in(thumbnail_articles.map(&:id)))
   end
 
   def writers
