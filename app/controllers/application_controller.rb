3bbbee6434f919ddccdc04e7e8d82bf0c03f26e8
John Gesimondo <john@jmondo.com>
2013-01-04 22:30:45 -0800

    don't reload rails admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,24 +10,4 @@ class ApplicationController < ActionController::Base
     redirect_to root_path
     return false
   end
-
-  before_filter :reload_rails_admin, if: :rails_admin_path?
-
-  private
-
-  def reload_rails_admin
-    models = %W(User UserProfile)
-    models.each do |m|
-      RailsAdmin::Config.reset_model(m)
-    end
-    RailsAdmin::Config::Actions.reset
-
-    load("#{Rails.root}/config/initializers/rails_admin.rb")
-  end
-
-  def rails_admin_path?
-    controller_path =~ /rails_admin/ && Rails.env == "development"
-  end
-
-
 end
