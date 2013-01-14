e77fa79d829a32709afeca8c2692a74467707f2e
John Gesimondo <john@jmondo.com>
2013-01-13 20:56:46 -0800

    new school page

--- app/controllers/school_articles_controller.rb
+++ app/controllers/school_articles_controller.rb
@@ -6,32 +6,18 @@ class SchoolArticlesController < ApplicationController
     instance_name: "article"
   belongs_to :school
 
-  helper_method :thumbnail_articles, :writers
+  helper_method :map_json
 
   protected
 
-  def thumbnail_articles
-    unless @thumbnail_articles
-      # first featured, first sponsored article, second featured article,
-      # fill others with regular articles, max 6
-      feature = end_of_association_chain.featured.limit(1)
-      sponsor = end_of_association_chain.sponsored.limit(1)
-      feature2 = end_of_association_chain.featured.offset(1).first
-      special_articles = [feature, sponsor, feature2].flatten.compact
-      qty_normal = 18 - special_articles.count
-      normal_articles = end_of_association_chain.published_no_flags.limit(qty_normal)
-      @thumbnail_articles ||= special_articles + normal_articles
-    end
-    @thumbnail_articles
-  end
-
   def collection
-    @articles ||= end_of_association_chain.published.
-      where(Article.arel_table[:id].not_in(thumbnail_articles.map(&:id)))
+    @articles ||= end_of_association_chain.published.limit(8)
   end
 
-  def writers
-    @writers ||= parent.users.accepted_invitation.order(:articles_count).reverse_order
+  def map_json
+    @json ||= end_of_association_chain.to_gmaps4rails do |article, marker|
+      marker.infowindow render_to_string(:partial => "/articles/gmaps_article", :locals => { :article => article })
+    end
   end
 
 end
