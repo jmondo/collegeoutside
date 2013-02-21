8ff19ffe39bb12464822317a7d3054b857387b8d
John Gesimondo <john@jmondo.com>
2013-02-20 21:44:11 -0800

    add moosejaw

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
   def sponsor_link(sponsor_name, file_name, url)
-    link_to image_tag("sponsors/#{file_name}", :class => "sponsor_link_image sponsor_name", :alt => "#{sponsor_name} logo"), url
+    link_to image_tag("sponsors/#{file_name}", :class => "sponsor_link_image #{sponsor_name}", :alt => "#{sponsor_name} logo"), url
   end
 end
